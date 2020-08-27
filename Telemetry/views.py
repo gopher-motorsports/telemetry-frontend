83bb0f33da04be671b58fbe5af3c11b6f5c3dd83
jrajan15 <johnny.rajan@gmail.com>
2020-08-27 18:21:44 -0500

    dropdown

--- Telemetry/views.py
+++ Telemetry/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+import requests
 
 @login_required
 def home(request):
-    return render(request, 'Telemetry/home.html', {})
+    
+    r = requests.get('http://localhost:5001/init')
+    print(r.text)
+    return render(request, 'Telemetry/home.html', {'r':r.text})
 
