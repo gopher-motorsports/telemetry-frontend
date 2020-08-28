6eb999fed0cb8bc1f33a0c995ab96a2b2be5fa5a
Henry O'Callaghan <ocall018@umn.edu>
2020-08-27 23:28:47 -0500

    [fix] adds example code for parsing string array to JSON object

--- Telemetry/views.py
+++ Telemetry/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 import requests
+import json
 
 @login_required
 def home(request):
     
     r = requests.get('http://localhost:5001/init')
-    print(r.text)
-    return render(request, 'Telemetry/home.html', {'r':r.text})
+    json_sensors = json.dumps(r.text)
+    return render(request, 'Telemetry/home.html', {'r':json_sensors})
 
