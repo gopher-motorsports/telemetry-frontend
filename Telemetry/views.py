f50523e016e80c4d1e5bc8eaeb2fbeb647f8264b
jrajan15 <johnny.rajan@gmail.com>
2020-08-29 11:50:17 -0500

    [feat] adds clickable dropdown to select sensor with variable amt of columns

--- Telemetry/views.py
+++ Telemetry/views.py
@@ -6,29 +6,25 @@ import json
 
 @login_required
 def home(request):
-    
+    num_cols = 5
     r = requests.get('http://localhost:5001/init')
     json_sensors = json.dumps(r.text)
     data = r.json()
-    test_dict = {
-        "sensor": data
-    }
-   # print(data[0].lower())
+    
     list_sensors = []
     def sensor_list():
         for sensor in data:
             s_name = requests.get("http://localhost:5001/sensors/?sensor=" + sensor + "&number=1")
             data_sensor = s_name.json()
+            
+            data_sensor[0]['sensor'] = sensor
             list_sensors.append(data_sensor[0])
     sensor_list()
-    sensor_names = requests.get("http://localhost:5001/sensors/?sensor=bat_volts&number=1")
-    names_list = sensor_names.json()
-    print("names_list", names_list)
-    names = names_list[0]['name']
-
     return render(request, 
                 'Telemetry/home.html', 
-                {'r':json_sensors, 
-                'n': test_dict['sensor'], 
-                'sensors': list_sensors})
+                {
+                'sensors': list_sensors,
+                'cols': num_cols,
+                'cols_list': range(num_cols)}
+                )
 
